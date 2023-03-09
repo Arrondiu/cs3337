37e59a031788603282456b36a732870e2d6d7c00
CindyIsCoding <126301489+CindyIsCoding@users.noreply.github.com>
2023-03-09 13:45:03 -0800

    Update Index.java

--- webtest/servlet/Index.java
+++ webtest/servlet/Index.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Index
- */
 @WebServlet("/Index")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,13 +14,15 @@ public class Index extends HttpServlet {
     public Index() {
         super();
     }
-
+	
+	// The doGet method gets information from the server
+	// In this method, it receives request from Index.jsp and forwards the data to doPost
+	// (NOT FINISHED) We are trying to make this into an order menu
+	// Currently, this just looks at the jsp
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/Index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 	}
-
 }
